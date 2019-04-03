04a2acd18233ef73972a81db04e825557ec97bb4
Davidagud <davidagud@gmail.com>
2019-04-03 18:18:11 -0500

    trying to fix deploy crashes

--- data/local.js
+++ data/local.js
@@ -1,8 +1,3 @@
-// Settings specific to this server. Change the URL
-// if you are deploying in production. Then copy to
-// data/local.js. That folder is shared by all
-// deployments in our stagecoach recipe
-
 module.exports = {
   modules: {
     'apostrophe-assets': {
