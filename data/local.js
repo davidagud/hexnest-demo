fd4e33ca09918766f9f8cce9efbe8f2978795a3c
Davidagud <davidagud@gmail.com>
2019-04-04 08:48:34 -0500

    got site to deploy and AWS S3 to connect

--- data/local.js
+++ data/local.js
@@ -1,13 +1,13 @@
 module.exports = {
   modules: {
-    'apostrophe-assets': {      
+    'apostrophe-assets': {
     // Set to true for full CSS and JS minify, on staging and production servers
     // minify: true
     },
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
     'apostrophe-db': {
-    uri: process.env.MONGODB_URI
+    // uri: process.env.MONGODB_URI
     }
   }
 };
