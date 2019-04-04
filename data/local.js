7727b1d776830697a3450e41301bd469a7d09d74
Davidagud <davidagud@gmail.com>
2019-04-03 19:59:58 -0500

    fixing deploy

--- data/local.js
+++ data/local.js
@@ -1,16 +1,13 @@
 module.exports = {
   modules: {
     'apostrophe-assets': {
-      // Set to true for full CSS and JS minify, on staging and production servers
-      // minify: true
+    // Set to true for full CSS and JS minify, on staging and production servers
+    // minify: true
     },
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
     'apostrophe-db': {
     //  uri: process.env.APOS_MONGODB_URI
-  },
-  'apostrophe-express': {
-      port: process.env.PORT
     }
   }
 };
