09a9a95a6961b4bb7cd7ed71831f30b2ee82deea
Davidagud <davidagud@gmail.com>
2019-04-07 20:03:05 -0500

    adding ssl cert

--- app.js
+++ app.js
@@ -57,8 +57,3 @@ var apos = require('apostrophe')({
 
   }
 });
-
-apos.app.get('/.well-known/acme-challenge/_eaRPBufwSesFDp5-MKPDgh7dJ2Qfz4ZsUIO2LvigAQ
-', function(req, res){
-  return res.send(_eaRPBufwSesFDp5-MKPDgh7dJ2Qfz4ZsUIO2LvigAQ.SKzb38UrsxwM0HK05wWRCFIsn774O0BNq8e57H3YSyk)
-});
