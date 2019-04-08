7fabfad75ec6fe6526d5f74f4b841fee979d65e2
Davidagud <davidagud@gmail.com>
2019-04-07 19:59:09 -0500

    adding ssl cert

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var path = require('path');
 
-self.apos.app.get('/.well-known/acme-challenge/_eaRPBufwSesFDp5-MKPDgh7dJ2Qfz4ZsUIO2LvigAQ
-', function(req, res){
-  return res.send(_eaRPBufwSesFDp5-MKPDgh7dJ2Qfz4ZsUIO2LvigAQ.SKzb38UrsxwM0HK05wWRCFIsn774O0BNq8e57H3YSyk)
-});
-
 var apos = require('apostrophe')({
   shortName: 'hexnest',
 
@@ -61,4 +56,9 @@ var apos = require('apostrophe')({
     'slideshow-widgets': { extend: 'apostrophe-widgets' }
 
   }
+});
+
+self.apos.app.get('/.well-known/acme-challenge/_eaRPBufwSesFDp5-MKPDgh7dJ2Qfz4ZsUIO2LvigAQ
+', function(req, res){
+  return res.send(_eaRPBufwSesFDp5-MKPDgh7dJ2Qfz4ZsUIO2LvigAQ.SKzb38UrsxwM0HK05wWRCFIsn774O0BNq8e57H3YSyk)
 });
