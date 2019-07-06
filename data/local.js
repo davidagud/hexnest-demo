e1ce508dff770908fda1daedb70c0ede5d26f309
davidagud <davidagud@gmail.com>
2019-07-06 14:03:00 -0500

    finished production set up

--- data/local.js
+++ data/local.js
@@ -2,7 +2,7 @@ module.exports = {
   modules: {
     'apostrophe-assets': {
     // Set to true for full CSS and JS minify, on staging and production servers
-    // minify: true
+      minify: true
     },
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
